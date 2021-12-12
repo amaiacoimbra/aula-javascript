0cf15b26822e7503018bc70f2bab40770a4871b1
Amaia <amaiacoimbra@gmail.com>
2021-12-12 00:38:29 -0300

    nova atualização

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
+/*
 var nome = 'Amaia Coimbra'
 var n1 = 22
 var n2 = 10
 var frase = "aba aba aba"
-
-//alert(nome + ' tem ' + idade + ' anos')
-
+alert(nome + ' tem ' + idade + ' anos')
 console.log(nome)
 console.log(n1 + n2)
 console.log(frase.replace("aba","eba"))
 console.log(frase.toLocaleLowerCase())
 console.log(n1 * n2)
+
+var lista = ["maça", 'pêra', "laranja"]
+lista.push("uva")
+lista.pop()
+
+console.log(lista[0])
+console.log(lista.toString()[0])
+console.log(lista.length)
+console.log(lista.reverse())
+console.log(lista.join(' - '))
+*/
+
+//var fruta = {nome:'maça', cor:'vermelha'}
+//console.log(fruta.nome)
+//alert('fruta.cor')
+
+/*
+var frutas = [{nome:'maça', cor:'vermelha'}, {nome:'uva', cor:'roxa'}]
+console.log(frutas)
+alert(frutas[1].nome)
+*/
+

c7740984c152e697bf3a793da452701ffcfc7d58
Amaia <amaiacoimbra@gmail.com>
2021-12-12 00:23:35 -0300

    atualização

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 var nome = 'Amaia Coimbra'
-var idade = 22
-var idade2 = 10
+var n1 = 22
+var n2 = 10
+var frase = "aba aba aba"
 
 //alert(nome + ' tem ' + idade + ' anos')
 
+console.log(nome)
+console.log(n1 + n2)
+console.log(frase.replace("aba","eba"))
+console.log(frase.toLocaleLowerCase())
+console.log(n1 * n2)

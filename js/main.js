894da83aed2af02a7e3b1c440d4baead9040e502
Amaia <amaiacoimbra@gmail.com>
2021-12-12 01:01:22 -0300

    atualização

--- js/main.js
+++ js/main.js
@@ -31,3 +31,32 @@ console.log(frutas)
 alert(frutas[1].nome)
 */
 
+/*
+var idade = prompt('Qual sua idade?')
+if (idade >= 18){
+    alert('maior de idade')
+} else {
+    alert('menor de idade')
+}
+*/
+
+/*
+var count = 0
+while (count <= 5){
+    console.log(count)
+    alert(count)
+    count ++
+}
+*/
+
+/*
+var count
+for(count=0; count <=5; count ++){
+    alert(count)
+}
+*/
+
+//var d = new Date()
+//alert(d.getDay())
+//alert(d.getHours())
+//alert(d.getMonth())
